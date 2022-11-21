8292bed69d2ead16b74c0e2fb2dfa0a394e85e16
Bismar Trujillo <bismar20@hotmail.com>
2022-11-21 15:48:25 -0500

    first commit

---
+++ scripts/mountainfinder.js
@@ -0,0 +1,71 @@
+"use strict";
+
+window.onload = init;
+
+function init() {
+    const mountainListBtn = document.getElementById("mountainListBtn");
+    mountainListBtn.onclick = mountainListBtnOnClick;
+    mountainDropdown();
+    mountainListBtnOnClick();
+}
+function informationCardMaker(mountain) {
+    let cardLocationContainer = document.getElementById("cardLocationContainer");
+
+    let card = document.createElement("div");
+    card.className = "card col-md-8 col-lg-8 m-5";
+    card.style.width = "800px";
+    cardLocationContainer.appendChild(card);
+
+    let cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+    card.appendChild(cardBody);
+
+    let img = document.createElement("img");
+    img.src = "images/"+mountainsArray[mountain].img;
+    img.alt = mountainsArray[mountain].name;
+    img.className = "card-img";
+    card.appendChild(img);
+
+
+    let title = document.createElement("h5");
+    title.className = "card-title";
+    title.innerText = mountainsArray[mountain].name;
+    cardBody.appendChild(title);
+
+    let text1 = document.createElement("p");
+    text1.className = "card-text";
+    text1.innerText = "Effort: " + mountainsArray[mountain].effort + "     Elevation: " + mountainsArray[mountain].elevation;
+    cardBody.appendChild(text1);
+
+    let text2 = document.createElement("p");
+    text2.className = "card-text";
+    text2.innerText = mountainsArray[mountain].desc;
+    cardBody.appendChild(text2);
+
+    let text3 = document.createElement("p");
+    text3.className = "card-text";
+    text3.innerText = "Coordinates: Lat: " + mountainsArray[mountain].coords.lat + "  Lng: " + mountainsArray[mountain].coords.lng;
+    cardBody.appendChild(text3);
+//<img src="..." class="card-img-top" alt="...">
+}
+function mountainDropdown() {
+    const mountainList = document.getElementById("mountainList");
+    let blankOption = document.createElement("option");
+    blankOption.textContent = "Select Location";
+    mountainList.appendChild(blankOption);
+
+    for (let mountain in mountainsArray) {
+        let optionMountain = document.createElement("option");
+        optionMountain.textContent = mountainsArray[mountain].name;
+        mountainList.appendChild(optionMountain);
+    }
+}
+function mountainListBtnOnClick() {
+    cardLocationContainer.innerHTML = "";
+    let selectedMountainOption = document.getElementById("mountainList").value;
+    for (let mountain in mountainsArray) {
+        if (selectedMountainOption == mountainsArray[mountain].name) {
+            informationCardMaker(mountain);
+        }
+    }
+}
---
+++ scripts/parkfinder.js
@@ -0,0 +1,93 @@
+"use strict";
+
+window.onload = init;
+
+function init() {
+    const locationListBtn = document.getElementById("locationListBtn");
+    const parkTypeListBtn = document.getElementById("parkTypeListBtn");
+    locationListBtn.onclick = searchByLocationOnClick;
+    parkTypeListBtn.onclick = parkTypeListBtnOnClick;
+
+    locationDropdown();
+    parkTypeDropdown();
+    searchByLocationOnClick();
+    parkTypeListBtnOnClick();
+}
+
+function locationDropdown() {
+    const searchByLocation = document.getElementById("searchByLocationList");
+    let blankOption = document.createElement("option");
+    blankOption.textContent = "Select Location";
+    searchByLocation.appendChild(blankOption);
+
+    for (let parks in locationsArray) {
+        let optionLocation = document.createElement("option");
+        optionLocation.textContent = locationsArray[parks];
+        searchByLocation.appendChild(optionLocation);
+    }
+}
+function parkTypeDropdown() {
+    const searchByParkType = document.getElementById("searchByParkTypeList");
+    let blankOption = document.createElement("option");
+    blankOption.textContent = "Select Park Type";
+    searchByParkType.appendChild(blankOption);
+
+    for (let parks in parkTypesArray) {
+        let optionParkType = document.createElement("option");
+        optionParkType.textContent = parkTypesArray[parks];
+        searchByParkType.appendChild(optionParkType);
+    }
+}
+function informationCardMaker(park) {
+    let cardLocationContainer = document.getElementById("cardLocationContainer");
+
+    let card = document.createElement("div");
+    card.className = "card col-sm-6 col-lg-3 m-4";
+    cardLocationContainer.appendChild(card);
+
+    let cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+    card.appendChild(cardBody);
+
+    let title = document.createElement("h5");
+    title.className = "card-title";
+    title.innerText = nationalParksArray[park].LocationName;
+    cardBody.appendChild(title);
+
+    let text1 = document.createElement("p");
+    text1.className = "card-text";
+    text1.innerText = nationalParksArray[park].Address;
+    cardBody.appendChild(text1);
+
+    let text2 = document.createElement("p");
+    text2.className = "card-text";
+    text2.innerText = nationalParksArray[park].City + ", " + nationalParksArray[park].State + " " + nationalParksArray[park].ZipCode;
+    cardBody.appendChild(text2);
+
+    let buttonSelectLocation = document.createElement("a")
+    buttonSelectLocation.className = "btn btn-dark ";
+    buttonSelectLocation.innerHTML = "Visit Us"
+    buttonSelectLocation.href = nationalParksArray[park].Visit;
+    cardBody.appendChild(buttonSelectLocation);
+
+}
+function searchByLocationOnClick() {
+    cardLocationContainer.innerHTML = "";
+    let selectedLocationOption = document.getElementById("searchByLocationList").value;
+    for (let park in nationalParksArray) {
+        if (selectedLocationOption == nationalParksArray[park].State) {
+            informationCardMaker(park);
+        }
+    }
+}
+function parkTypeListBtnOnClick() {
+    cardLocationContainer.innerHTML = "";
+    let selectedParkTypeOption = document.getElementById("searchByParkTypeList").value;
+    for (let park in nationalParksArray) {
+        if ((nationalParksArray[park].LocationName).indexOf(selectedParkTypeOption) != -1) {
+            informationCardMaker(park);
+        }
+    }
+}
+
+
